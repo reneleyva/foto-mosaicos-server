bcb4b264c126d04ac95790ef076e3e8762cf0972
Renoir <sinmatonesaporfavor@gmail.com>
2020-05-17 14:16:05 -0500

    testing

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if request.method == 'POST':
+        photo = request.files['photo']
+        return render_template('test.html')
